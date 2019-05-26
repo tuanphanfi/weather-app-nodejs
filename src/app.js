717ffaf4c1e4942061a9c98c6c4b6836c9d5426b
tuanphan.fi@gmail.com <tuanphan.fi@gmail.com>
2019-05-26 13:38:28 +0300

    .gitinorge, basic configuration(p1)

--- src/app.js
+++ src/app.js
@@ -1,16 +1,13 @@
 const express = require('express')
 const path = require('path')
 
-console.log(__filename)
-
 const app = express()
-
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-app.get('', (req, res) => {
-    res.send("")
+app.get('/', (req, res) => {
+    res.send("a")
 })
 
 app.get('/help', (req, res) => {
