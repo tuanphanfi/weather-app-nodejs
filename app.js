0094b6519fba6649cb1bdf7b0f9ad1dbb373534c
tuanphan.fi@gmail.com <tuanphan.fi@gmail.com>
2019-05-26 13:26:29 +0300

    .gitinorge, basic configuration(p1)

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const path = require('path')
+
+console.log(__filename)
+
+const app = express()
+
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.send("")
+})
+
+app.get('/help', (req, res) => {
+    res.send([
+        {
+            name: "Tuan Phan",
+            email: "tuanphan.fi@gmail.com"
+        },
+        {
+            name: "Tuan Phan",
+            email: "tuanphan.fi@gmail.com"
+        }
+    ])
+})
+
+app.get('/about', (req, res) => {
+    res.send('<h1>About</h1>')
+})
+
+app.get('/weather', (req, res) => {
+    res.send('weather')
+})
+
+app.listen(3000, () => console.log("Running on port 3000"));
